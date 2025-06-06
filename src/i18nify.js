1e8250785d1d708bcccf35bf6aa27bbe3e1087b0
bruno-gomes97 <gomes_bruno97@hotmail.com>
2025-06-06 17:08:58 -0300

    feat(tas01): criar padrões p cores, tamanho, espaçamentos e add dependências

---
+++ src/i18nify.js
@@ -0,0 +1,37 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import enTranslation from './locales/en.json';
+import esARTranslation from './locales/es-AR.json';
+import ptTranslation from './locales/pt.json';
+
+const resources = {
+	en: {
+		translation: enTranslation,
+	},
+	pt: {
+		translation: ptTranslation,
+	},
+	'es-AR': {
+		translation: esARTranslation,
+	},
+};
+
+const savedLng = localStorage.getItem("i18nextLng") || 'pt';
+
+i18n
+	.use(initReactI18next)
+	.init({
+		resources,
+		debug: true,
+		lng: savedLng,
+		fallbackLng: 'pt',
+		interpolation: {
+			escapeValue: false,
+		},
+		react: {
+			wait: true,
+		}
+	});
+
+export default i18n;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './i18nify.js'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
